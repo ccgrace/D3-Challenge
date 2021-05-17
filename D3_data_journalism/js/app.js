24365f12dee6f93c26ec289f5a969babbbefff2f
ccgrace <77289566+ccgrace@users.noreply.github.com>
2021-05-17 19:16:38 -0400

    wrote code

--- D3_data_journalism/js/app.js
+++ D3_data_journalism/js/app.js
@@ -13,7 +13,7 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select(".chart")
+var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -21,12 +21,57 @@ var svg = d3.select(".chart")
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// Import data 
+// Import data and parse data/cast as numbers
 d3.csv("../data/data.csv").then(function(censusData) {
-    console.log(censusData);
     censusData.forEach(function(data) {
-        data.abbr = data.abbr
         data.smokes = +data.smokes;
         data.age = +data.age;
     });
+
+    // Create scale functions 
+    var xLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(censusData, d => d.age)])
+        .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, de.max(censusData, d => d.smokes)])
+        .range([height, 0]);
+
+    // Create axis functions 
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+    
+    // Append axes to the chart 
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
+
+    // Create circles
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(censusData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.age))
+    .attr("cy", d => yLinearScale(d.smokes))
+    .attr("r", "10")
+    .attr("fill", "99ccff")
+
+    // Create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Number of Smokers");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("Age");
+}).catch(function(error) {
+    console.log(error);
 });
