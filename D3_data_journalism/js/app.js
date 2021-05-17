ce8acc596055fde4c93b5d604aa2e70e574cfc9c
ccgrace <77289566+ccgrace@users.noreply.github.com>
2021-05-17 19:25:41 -0400

    changed marker color and linear scale

--- D3_data_journalism/js/app.js
+++ D3_data_journalism/js/app.js
@@ -22,7 +22,8 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import data and parse data/cast as numbers
-d3.csv("../data/data.csv").then(function(censusData) {
+d3.csv("D3_data_journalism/data/data.csv").then(function(censusData) {
+    console.log(censusData)
     censusData.forEach(function(data) {
         data.smokes = +data.smokes;
         data.age = +data.age;
@@ -30,11 +31,11 @@ d3.csv("../data/data.csv").then(function(censusData) {
 
     // Create scale functions 
     var xLinearScale = d3.scaleLinear()
-        .domain([0, d3.max(censusData, d => d.age)])
+        .domain([30, d3.max(censusData, d => d.age)])
         .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-        .domain([0, de.max(censusData, d => d.smokes)])
+        .domain([8, d3.max(censusData, d => d.smokes)])
         .range([height, 0]);
 
     // Create axis functions 
@@ -56,8 +57,8 @@ d3.csv("../data/data.csv").then(function(censusData) {
     .append("circle")
     .attr("cx", d => xLinearScale(d.age))
     .attr("cy", d => yLinearScale(d.smokes))
-    .attr("r", "10")
-    .attr("fill", "99ccff")
+    .attr("r", "15")
+    .attr("fill", "lightblue")
 
     // Create axes labels
     chartGroup.append("text")
